c7ef157e9eb721f230b0751a17e592315636714e
Akuma <aeres99@hotmail.com>
2020-04-16 04:17:48 +0000

    Added the files

---
+++ Hello.java
@@ -0,0 +1,6 @@
+class Hello{
+	public static void main(String args[]){
+System.out.println("Hello world!");
+System.out.print("I am made using jenkins");
+}}
+
---
+++ runnercode.py
@@ -0,0 +1,6 @@
+a = 1000
+b = 10000
+c = a ** b
+print(c)
+print("The ** operator in python3 is basically to the power of operator")
+
